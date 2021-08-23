20c5446fb3d92ee2363244471db10a49602e1ba5
Ethan Reesor <ethan.reesor@gmail.com>
2021-08-23 14:40:34 -0500

    interp: improve breakpoints
    Generalize SetBreakpoints, add function breakpoint support, track which
    Program is associated with a root node.

--- interp/interp.go
+++ interp/interp.go
@@ -25,32 +25,46 @@ import (
 
 // Interpreter node structure for AST and CFG.
 type node struct {
-	child  []*node        // child subtrees (AST)
-	anc    *node          // ancestor (AST)
-	start  *node          // entry point in subtree (CFG)
-	tnext  *node          // true branch successor (CFG)
-	fnext  *node          // false branch successor (CFG)
-	interp *Interpreter   // interpreter context
-	frame  *frame         // frame pointer used for closures only (TODO: suppress this)
-	index  int64          // node index (dot display)
-	findex int            // index of value in frame or frame size (func def, type def)
-	level  int            // number of frame indirections to access value
-	nleft  int            // number of children in left part (assign) or indicates preceding type (compositeLit)
-	nright int            // number of children in right part (assign)
-	kind   nkind          // kind of node
-	pos    token.Pos      // position in source code, relative to fset
-	sym    *symbol        // associated symbol
-	typ    *itype         // type of value in frame, or nil
-	recv   *receiver      // method receiver node for call, or nil
-	types  []reflect.Type // frame types, used by function literals only
-	scope  *scope         // frame scope
-	action action         // action
-	exec   bltn           // generated function to execute
-	gen    bltnGenerator  // generator function to produce above bltn
-	val    interface{}    // static generic value (CFG execution)
-	rval   reflect.Value  // reflection value to let runtime access interpreter (CFG)
-	ident  string         // set if node is a var or func
-	bkp    bool           // breakpoint
+	child     []*node        // child subtrees (AST)
+	anc       *node          // ancestor (AST)
+	start     *node          // entry point in subtree (CFG)
+	tnext     *node          // true branch successor (CFG)
+	fnext     *node          // false branch successor (CFG)
+	interp    *Interpreter   // interpreter context
+	frame     *frame         // frame pointer used for closures only (TODO: suppress this)
+	index     int64          // node index (dot display)
+	findex    int            // index of value in frame or frame size (func def, type def)
+	level     int            // number of frame indirections to access value
+	nleft     int            // number of children in left part (assign) or indicates preceding type (compositeLit)
+	nright    int            // number of children in right part (assign)
+	kind      nkind          // kind of node
+	pos       token.Pos      // position in source code, relative to fset
+	sym       *symbol        // associated symbol
+	typ       *itype         // type of value in frame, or nil
+	recv      *receiver      // method receiver node for call, or nil
+	types     []reflect.Type // frame types, used by function literals only
+	scope     *scope         // frame scope
+	action    action         // action
+	exec      bltn           // generated function to execute
+	gen       bltnGenerator  // generator function to produce above bltn
+	val       interface{}    // static generic value (CFG execution)
+	rval      reflect.Value  // reflection value to let runtime access interpreter (CFG)
+	ident     string         // set if node is a var or func
+	breakPos  bool           // breakpoint set by position
+	breakCall bool           // breakpoint set on function call
+	prog      *Program       // program this is part of, only stored on the root node
+}
+
+func (n *node) shouldBreak() bool {
+	if n == nil {
+		return false
+	}
+
+	if n.breakPos || n.breakCall {
+		return true
+	}
+
+	return false
 }
 
 // receiver stores method receiver object access path.
--- interp/program.go
+++ interp/program.go
@@ -22,7 +22,7 @@ func (interp *Interpreter) Compile(src string) (*Program, error) {
 
 // CompilePath parses and compiles a Go code located at the given path.
 func (interp *Interpreter) CompilePath(path string) (*Program, error) {
-	if !isFile(path) {
+	if !isFile(interp.filesystem, path) {
 		_, err := interp.importSrc(mainID, path, NoTest)
 		return nil, err
 	}
@@ -104,7 +104,9 @@ func (interp *Interpreter) compile(src, name string, inc bool) (*Program, error)
 		root.cfgDot(dotWriter(dotCmd))
 	}
 
-	return &Program{pkgName, root, initNodes}, nil
+	prog := &Program{pkgName, root, initNodes}
+	root.prog = prog
+	return prog, nil
 }
 
 // Execute executes compiled Go code.
